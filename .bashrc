6cf07e8ca41fd16a625b010dd82cb1dea7a6de5b
Kee Hoon <kl546@cornell.edu>
2018-03-18 12:42:49 -0400

    name, netid

---
+++ .bashrc
@@ -0,0 +1,1 @@
+source /Users/keehoon/Desktop/CS4300/final/CS4300_Flask_template/venv/bin/activate.sh
--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,9 +1,9 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Foodies : The Food Hunters of NYC"
+net_id = "Jae Min Cha : jc988, Woo Kyung Lee : wl357, Jung Yun Park : jp862, Kee Hoon Lee : kl546"
 
 @irsystem.route('/', methods=['GET'])
 def search():
@@ -15,6 +15,3 @@ def search():
 		output_message = "Your search: " + query
 		data = range(5)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-
-
-
