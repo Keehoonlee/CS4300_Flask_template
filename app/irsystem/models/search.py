4fcf1cfe36437942f1f3652a32ffb7f1bb08cb34
Jae Cha <jaecha@dhcp-rhodes-28.redrover.cornell.edu>
2018-04-12 11:28:41 -0400

    Completed first draft of search.py. Needs to incorporate it in search_controller.py.

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -1,4 +1,5 @@
 import sentiment_analysis
+from collections import defaultdict
 
 def compare_timelimit_timeposted(time_limit, time_posted):
     """Returns true if the review's time posted satisfies the user input's time limit"""
@@ -55,4 +56,59 @@ def filter_neg_reviews(reviews):
 def compute_percentage_per_type(reviews):
     """Computes the percentages of the reviews for each type
 
-    Returns a dictionary in format {\cuisine type: percentage}"""
+    Returns a dictionary in format {\cuisine type: percentage}
+
+    [reviews]: list of JSON objects"""
+
+    n_reviews = len(reviews)
+    percentage_dict = defaultdict(int)
+
+    for review in reviews:
+        percentage_dict[review["restaurant_type"]] += 1
+
+    for type in percentage_dict.keys():
+        percentage_dict[type] /= n_reviews
+
+    return percentage_dict
+
+###############################COMPUTING TOP RESTAURANTS PER TYPE##################################
+def filter_reviews_type(reviews,type):
+    """Filter [reviews] by [type].
+    Returns a list of JSON objects."""
+
+    reviews_of_type = []
+    for review in reviews:
+        if (review["restaurant_type"]==type):
+            reviews_of_type.append(review)
+
+    return reviews_of_type
+
+def compute_top_rest(reviews):
+    """Computes the top restaurant for [reviews].
+    Returns a sorted list of restaurants in the order of decreasing popularity."""
+
+    rest_stars_dict = defaultdict(int)
+
+    for review in reviews:
+        rest_stars_dict[review["restaurant"]] += review["stars_given"]
+
+    ranked_rest_lst = [k for k in sorted(rest_stars_dict, key=rest_stars_dict.get, reverse=True)]
+
+    return ranked_rest_lst
+
+def compute_top_rest_per_type(reviews):
+    """Computes the top restaurant list for each type in [reviews] that has
+    review percentage over 1%
+
+    Returns a dicitonary in format {\cuisine type: lst of restaurants}"""
+
+    rest_per_type_dict = defaultdict(list)
+    percentages = compute_percentage_per_type(reviews)
+
+    for type in percentages.keys():
+        if percentages[type] >= 0.01:
+            reviews_of_type = filter_reviews_type(reviews,type)
+            top_rest_list = compute_top_rest(reviews_of_type)
+            rest_per_type_dict[type] = top_rest_list
+
+    return rest_dict
